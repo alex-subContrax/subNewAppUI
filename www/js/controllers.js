8e579affbd73806eb80721f2ec8ae8cf197391b7
Alex Milman <alex@miyasolutions.com>
2015-11-26 20:48:21 +0200

    First Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,47 @@
+angular.module('app.controllers', [])
+  
+.controller('signupToSubContraXCtrl', function($scope) {
+
+})
+   
+.controller('loginToSubContraXCtrl', function($scope) {
+
+})
+   
+.controller('dashboardCtrl', function($scope) {
+
+})
+   
+.controller('jobsCtrl', function($scope) {
+
+})
+   
+.controller('myNetworkCtrl', function($scope) {
+
+})
+      
+.controller('customersCtrl', function($scope) {
+
+})
+ 
+.controller('settingsCtrl', function($scope) {
+
+})
+
+.controller('addAJobCtrl', function($scope, $ionicHistory) {
+    $scope.myGoBack = function() {
+    $ionicHistory.goBack();
+  };
+})
+ 
+.controller('addAContractorCtrl', function($scope, $ionicHistory) {
+        $scope.myGoBack = function() {
+    $ionicHistory.goBack();
+  };
+})
+
+.controller('addACustomerCtrl', function($scope, $ionicHistory) {
+        $scope.myGoBack = function() {
+    $ionicHistory.goBack();
+  };
+})
