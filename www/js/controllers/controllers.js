6475c5db49431ef18b86f639fe7cfe965f030ad0
Alex Milman <alex@miyasolutions.com>
2015-11-27 12:06:34 +0200

    Changed Directories

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -45,3 +45,5 @@ angular.module('app.controllers', [])
     $ionicHistory.goBack();
   };
 })
+
+
---
+++ www/js/controllers/resetPasswordPromptCtrl.js
@@ -0,0 +1,28 @@
+angular.module('app.controllers').controller('resetPasswordPromptCtrl', function($scope, $ionicPopup) {
+     $scope.showResetPasswordPopup = function() {
+  $scope.data = {}
+
+  // An elaborate, custom popup
+  var myPopup = $ionicPopup.show({
+    template: '<input type="email" ng-model="data.password">',
+    title: 'Please enter your email',
+    subTitle: 'In order to reset your password',
+    scope: $scope,
+    buttons: [
+      { text: 'Cancel' },
+      {
+        text: '<b>Send</b>',
+        type: 'button-positive',
+        onTap: function(e) {
+          if (!$scope.data.password) {
+            //don't allow the user to close unless he enters an email 
+            e.preventDefault();
+          } else {
+            return $scope.data.wifi;
+              }
+            }
+          }
+        ]
+      });
+    };
+})
