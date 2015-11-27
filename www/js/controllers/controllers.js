2a17ff350f56a88ace91be26ad9a6642364399b6
Alex Milman <alex@miyasolutions.com>
2015-11-27 13:37:47 +0200

    Nothing Important

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -1,7 +1,18 @@
 angular.module('app.controllers', [])
   
-.controller('signupToSubContraXCtrl', function($scope) {
-
+.controller('signupToSubContraXCtrl', function($scope, $ionicPopup, $state, signUpService) {
+    $scope.data = {};
+ 
+    $scope.signUp = function() {
+        signUpService.signUpUser($scope.data.email, $scope.data.password).success(function(data) {
+            $state.go('tabsController.dashboard');
+        }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Sign Up failed!',
+                template: 'Please input a valid email!'
+            });
+        });
+    }
 })
    
 .controller('loginToSubContraXCtrl', function($scope) {
--- www/js/services/services.js
+++ www/js/services/services.js
@@ -4,7 +4,27 @@ angular.module('app.services', [])
 
 }])
 
-.service('BlankService', [function(){
-
+.service('signUpService', [function($q) {
+    return {
+        signUpUser: function(email, pw) 
+        {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+ 
+            if (email == 'alex@alex.com' && pw == '1234') {
+                deferred.resolve('Welcome ' + email + '!');
+            } else {
+                deferred.reject('Wrong credentials.');
+            }
+            promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+        }
+    }
 }]);
-
